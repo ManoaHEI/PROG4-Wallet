5c5d1128824326ec5fe8be9d64fa66d880939005
ManoaHEI <hei.manoa.4@gmail.com>
2024-03-20 13:54:06 +0100

    init project

---
+++ src/main/java/com/prog4/wallet/Prog4WalletApplication.java
@@ -0,0 +1,13 @@
+package com.prog4.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog4WalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prog4WalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prog4/wallet/Prog4WalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prog4.wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog4WalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
